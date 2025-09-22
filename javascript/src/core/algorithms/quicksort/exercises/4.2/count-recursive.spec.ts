37f9f21450041cf28c6cdc82b931a5a9ff09419c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-09-21 21:15:53 -0300

    move recursion examples to the correct folder

--- javascript/src/core/algorithms/quicksort/exercises/4.2/count-recursive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { countRecursive } from "./count-recursive";
-
-describe("", () => {
-  it("", async () => {
-    const result = countRecursive([3, 7, 9, 14]);
-
-    expect(result).toBe(4);
-  });
-});
---
+++ javascript/src/core/concepts/recursion/find-max-number-recursive.spec.ts
@@ -0,0 +1,9 @@
+import { findMaxNumberRecursive } from './find-max-number-recursive';
+
+describe('findMaxNumber', () => {
+  it('', async () => {
+    const result = findMaxNumberRecursive([3, 49, 9, 14]);
+
+    expect(result).toBe(49);
+  });
+});
